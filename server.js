c2cd6a7f5ebaeb08c081d9a0f8fd6fb7294d3f0b
Suraj Yadav <2018surajyadav96@gmail.com>
2021-03-29 13:14:39 +0530

    master - Hreoku deployment fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.all('*', (req, res) => {
+app.all('/*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
